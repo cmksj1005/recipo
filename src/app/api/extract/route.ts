99c952393fcd49352064fa2b0f2dcd5d7852d098
Joe <joe@mac.home>
2026-04-13 10:26:30 -0400

    succeeded to print recipe using chatgpt

--- src/app/api/extract/route.ts
+++ src/app/api/extract/route.ts
@@ -3,25 +3,36 @@
 // =====================================================================
 
 import { fetchTranscript } from 'youtube-transcript';
-// import OpenAI from 'openai';
+import OpenAI from 'openai';
 // This 'client' is the thing you use to talk to OpenAI
-// const client = new OpenAI();
+const client = new OpenAI();
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    // extract transcript from the youtube video
+    // it returns an array of objects
     const transcript = await fetchTranscript(body.url);
+    let transcriptTextParts = '';
 
-    console.log(transcript);
+    // concatenate all the texts from the transcript
+    for (const item of transcript) {
+      transcriptTextParts += item.text;
+      // Better code: const transcriptTextParts = transcript.map(item => item.text).join(' ');
+    }
 
-    // const response = await client.responses.create({
-    //   model: 'gpt-4o-mini',
-    //   input: transcript.text,
-    // });
+    console.log(transcriptTextParts);
 
-    // return Response.json({
-    //   result: response.output_text,
-    // });
+    const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input:
+        transcriptTextParts +
+        "extract the recipe if this content has it. If not, return 'It is not cook youtube video' Do not return anything else. displayed language should be english",
+    });
+
+    return Response.json({
+      result: response.output_text,
+    });
   } catch (err) {
     console.error(err);
     return Response.json({ error: 'Server error' }, { status: 500 });
