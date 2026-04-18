e94bba761af0afc43872d1c9c50e4ba29029125d
Joe <joe@mac.home>
2026-04-18 11:34:06 -0400

    finished to make responsive desing for the search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,32 +47,33 @@ export default function Home() {
       <h1>Recipo</h1>
       <form className={styles.searchbarForm} onSubmit={handleSubmit}>
         {/* search bar */}
-        <div className={styles.searchWrapper}>
+        <div className={styles.searchbarWrapper}>
           <input
             className={styles.searchBar}
             type="text"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
-        </div>
-        {/* search button */}
-        <div className={styles.searchButtonWrapper}>
-          <button className={styles.submitButton} type="submit">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-8 text-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </button>
+
+          {/* search button */}
+          <div className={styles.searchButtonWrapper}>
+            <button className={styles.submitButton} type="submit">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-8 text-gray-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </form>
       {/* if I don't check whether result exists first, it returns error */}
