6ffc3e3bb458c28e9ecb0884f9be8060a27fdf72
Joe <joe@mac.home>
2026-04-21 14:04:51 -0400

    finished to make the user guide part of the main page

--- src/app/api/extract/route.ts
+++ src/app/api/extract/route.ts
@@ -60,117 +60,130 @@ export async function POST(req: Request) {
       Honey Chicken|chicken (500g), salt (1 tsp), oil (2 tbsp)|season the chicken with salt#heat oil over medium heat for 5 minutes#fry the chicken for 10 minutes until golden
       `;
 
-    //To get id from youtube link ex)https://www.youtube.com/watch?v=1EOmjfSAjw0, id = 1EOmjfSAjw0
-    //Got this code from stack overflow(https://stackoverflow.com/questions/33249105/embed-youtube-video-from-url)
-    function getVideoId(urlString: string) {
-      if (urlString && URL.canParse(urlString)) {
-        const url = new URL(urlString); // you can access parts like url.hostname, url.pathname, url.searchParams.
-        // shorts URL
-        if (urlString.includes('/shorts/')) {
-          return body.url.split('/shorts/')[1];
-        }
-        // short version URL
-        if (url.hostname === 'www.youtu.be' || url.hostname === 'youtu.be') {
-          const videoId: string = url.pathname.startsWith('/')
-            ? url.pathname.substring(1)
-            : url.pathname;
-          return videoId;
-        }
-        // regular version URL
-        const v = url.searchParams.get('v'); // ex) https://www.youtube.com/watch?v=abc123 / you can get abc123 using this code.
-        if (
-          (url.hostname === 'www.youtube.com' ||
-            url.hostname === 'youtube.com') &&
-          v
-        ) {
-          return v;
-        }
-        // if user enters wrong url
-        console.log('Error from getVideoId function');
+    function isValidUrl(urlString: string): boolean {
+      try {
+        new URL(urlString);
+        return true;
+      } catch (e) {
+        return false;
       }
     }
 
-    const videoId = getVideoId(body.url);
-
-    if (!videoId) {
-      return Response.json({ error: 'Invalid YouTube URL' }, { status: 400 });
-    }
-
-    // extract transcript from the youtube video
-    // it returns an array of objects
-    const transcript: TranscriptResponse[] = await fetchTranscript(videoId);
-
-    let transcriptTextParts = '';
-
-    // concatenate all the texts from the transcript
-    for (const item of transcript) {
-      transcriptTextParts += item.text;
-      // Better code: const transcriptTextParts = transcript.map(item => item.text).join(' ');
-    }
-
-    // get answer from chatgpt
-    const response = await client.responses.create({
-      model: 'gpt-4o',
-      input: transcriptTextParts + instructionForGpt,
-    });
-
-    const gptAnswer = response.output_text;
-
-    console.log(gptAnswer);
-
-    const splitGptAnswer = gptAnswer.split('|');
-    const recipeTitle = splitGptAnswer[0];
-    const allRecipeIngredients = splitGptAnswer[1].split(',');
-
-    const recipeIngredients: RecipeIngredients[] = [];
-
-    // get name, quantity, unit from ingredients sentence from chat gpt
-    function parseIngredients(ingredients: string[]) {
-      for (const ingredient of ingredients) {
-        const ingreName = ingredient.split('(')[0].replace(')', '').trim();
-        const ingreQuantity = ingredient.split('(')[1];
-
-        const parsedQuantity = ingreQuantity
-          ? ingreQuantity.replace(')', '').trim()
-          : 'N';
-
-        const match = parsedQuantity.match(/^(\d+(?:\.\d+)?)\s*([a-zA-Z]*)/);
-
-        const recipeQuantity: number | null = match?.[1]
-          ? Number(match[1])
-          : null;
-        const recipeUnit: string = match?.[2] ?? '';
-
-        recipeIngredients.push({
-          name: ingreName,
-          quantity: recipeQuantity,
-          unit: recipeUnit,
-        });
+    if (isValidUrl(body.url)) {
+      //To get id from youtube link ex)https://www.youtube.com/watch?v=1EOmjfSAjw0, id = 1EOmjfSAjw0
+      //Got this code from stack overflow(https://stackoverflow.com/questions/33249105/embed-youtube-video-from-url)
+      function getVideoId(urlString: string) {
+        if (urlString && URL.canParse(urlString)) {
+          const url = new URL(urlString); // you can access parts like url.hostname, url.pathname, url.searchParams.
+          // shorts URL
+          if (urlString.includes('/shorts/')) {
+            return body.url.split('/shorts/')[1];
+          }
+          // short version URL
+          if (url.hostname === 'www.youtu.be' || url.hostname === 'youtu.be') {
+            const videoId: string = url.pathname.startsWith('/')
+              ? url.pathname.substring(1)
+              : url.pathname;
+            return videoId;
+          }
+          // regular version URL
+          const v = url.searchParams.get('v'); // ex) https://www.youtube.com/watch?v=abc123 / you can get abc123 using this code.
+          if (
+            (url.hostname === 'www.youtube.com' ||
+              url.hostname === 'youtube.com') &&
+            v
+          ) {
+            return v;
+          }
+          // if user enters wrong url
+          console.log('Error from getVideoId function');
+        }
       }
+
+      const videoId = getVideoId(body.url);
+
+      if (!videoId) {
+        return Response.json({ error: 'Invalid YouTube URL' }, { status: 400 });
+      }
+
+      // extract transcript from the youtube video
+      // it returns an array of objects
+      const transcript: TranscriptResponse[] = await fetchTranscript(videoId);
+
+      let transcriptTextParts = '';
+
+      // concatenate all the texts from the transcript
+      for (const item of transcript) {
+        transcriptTextParts += item.text;
+        // Better code: const transcriptTextParts = transcript.map(item => item.text).join(' ');
+      }
+
+      // get answer from chatgpt
+      const response = await client.responses.create({
+        model: 'gpt-4o',
+        input: transcriptTextParts + instructionForGpt,
+      });
+
+      const gptAnswer = response.output_text;
+
+      console.log(gptAnswer);
+
+      const splitGptAnswer = gptAnswer.split('|');
+      const recipeTitle = splitGptAnswer[0];
+      const allRecipeIngredients = splitGptAnswer[1].split(',');
+
+      const recipeIngredients: RecipeIngredients[] = [];
+
+      // get name, quantity, unit from ingredients sentence from chat gpt
+      function parseIngredients(ingredients: string[]) {
+        for (const ingredient of ingredients) {
+          const ingreName = ingredient.split('(')[0].replace(')', '').trim();
+          const ingreQuantity = ingredient.split('(')[1];
+
+          const parsedQuantity = ingreQuantity
+            ? ingreQuantity.replace(')', '').trim()
+            : 'N';
+
+          const match = parsedQuantity.match(/^(\d+(?:\.\d+)?)\s*([a-zA-Z]*)/);
+
+          const recipeQuantity: number | null = match?.[1]
+            ? Number(match[1])
+            : null;
+          const recipeUnit: string = match?.[2] ?? '';
+
+          recipeIngredients.push({
+            name: ingreName,
+            quantity: recipeQuantity,
+            unit: recipeUnit,
+          });
+        }
+      }
+
+      parseIngredients(allRecipeIngredients);
+
+      console.log(recipeIngredients);
+
+      const recipeInstruction = splitGptAnswer[2].split('#');
+
+      // get embed url
+      function youtubeUrlToEmbed(id: string) {
+        const embedUrl = `https://www.youtube.com/embed/${id}`;
+        return embedUrl;
+      }
+
+      const recipeResult: RecipeResult = {
+        title: recipeTitle,
+        ingredients: recipeIngredients,
+        instruction: recipeInstruction,
+        embedUrl: youtubeUrlToEmbed(videoId),
+      };
+
+      return Response.json({
+        recipeResult,
+      });
+    } else {
+      console.log('not proper url');
     }
-
-    parseIngredients(allRecipeIngredients);
-
-    console.log(recipeIngredients);
-
-    const recipeInstruction = splitGptAnswer[2].split('#');
-
-    // get embed url
-    function youtubeUrlToEmbed(id: string) {
-      const embedUrl = `https://www.youtube.com/embed/${id}`;
-      return embedUrl;
-    }
-
-    const recipeResult: RecipeResult = {
-      title: recipeTitle,
-      ingredients: recipeIngredients,
-      instruction: recipeInstruction,
-      embedUrl: youtubeUrlToEmbed(videoId),
-    };
-
-    return Response.json({
-      recipeResult,
-    });
   } catch (err) {
     console.error(err);
     return Response.json({ error: 'Server error' }, { status: 500 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,12 +100,94 @@ export default function Home() {
       {/* display loading spinner animation when it is loading */}
 
       {!loading && !result && (
-        <div className={styles.initialMessage}>
-          <p>Paste a YouTube cooking URL to extract the recipe.</p>
+        <div className={styles.userGuideWrapper}>
+          <div className={styles.userGuide}>
+            <div className={styles.guideSection}>
+              <div className={styles.guideTitle}>
+                <Image
+                  src="/icons/questionIcon.png"
+                  alt="question mark icon"
+                  width={70}
+                  height={25}
+                  priority
+                ></Image>
+                <h2>How to Use Recipo</h2>
+              </div>
+              <ul>
+                <li>
+                  Paste a <strong>YouTube cooking video URL</strong> into the
+                  search bar and submit.
+                </li>
+                <li>
+                  The app will automatically extract and generate a structured
+                  recipe from the video.
+                </li>
+              </ul>
+            </div>
+            <div className={styles.guideSection}>
+              <div className={styles.guideTitle}>
+                <Image
+                  src="/icons/starIcon.png"
+                  alt="question mark icon"
+                  width={70}
+                  height={25}
+                  priority
+                ></Image>
+                <h2>Important Notes</h2>
+              </div>
+              <ul>
+                <li>
+                  Recipes are generated using{' '}
+                  <strong>AI based on the video’s transcript</strong>.
+                </li>
+                <li>
+                  If the video{' '}
+                  <strong>does not include spoken instructions </strong>
+                  or{' '}
+                  <strong>
+                    does not have an available transcript (including
+                    auto-generated captions)
+                  </strong>
+                  , the recipe may not be displayed or may be incomplete.
+                </li>
+                <li>
+                  The accuracy of the recipe depends on the{' '}
+                  <strong>quality and clarity of the transcript</strong>.
+                </li>
+              </ul>
+            </div>
+            <div className={styles.guideSection}>
+              <div className={styles.guideTitle}>
+                <Image
+                  src="/icons/conversationIcon.png"
+                  alt="question mark icon"
+                  width={70}
+                  height={25}
+                  priority
+                ></Image>
+                <h2>Feedback & Support</h2>
+              </div>
+              <ul>
+                <li>
+                  If you encounter any{' '}
+                  <strong>errors, bugs, or unexpected results</strong>, please
+                  let me know.
+                </li>
+                <li>
+                  Your feedback is extremely valuable and helps improve the
+                  application.
+                </li>
+                <li>
+                  I appreciate your support in making this tool better for
+                  everyone.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       )}
       {loading && (
-        <div className={styles.loadingWrapperki}>
+        <div className={styles.loadingWrapper}>
           <Lottie
             animationData={pizzaLoading}
             loop={true}
@@ -125,7 +207,7 @@ export default function Home() {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
             ></iframe>
           </div>
-          <h2>{result.title}</h2>
+          <h2 className={styles.recipeTitle}>{result.title}</h2>
           <div className={styles.recipeWrapper}>
             <div className={styles.ingredientsWrapper}>
               <h3 className={styles.sectionHeading}>Ingredients</h3>
