37c794d54d047cb8c4f42a4d8710254d619b7412
Joe <cmksj1005@gmail.com>
2026-04-26 21:19:53 -0400

    now recipe page displays the result and now I am working on the warning notification in the recipe page

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,13 +1,53 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { RecipeResult } from '@/types/recipe';
 import Logo from '@/components/recipe/Logo';
+import Result from '@/components/recipe/Result';
+import NotificationModal from '@/components/modals/NotificationModal';
+import Image from 'next/image';
 // import Searchbar from '@/components/recipe/Searchbar';
 
 export default function RecipePage() {
+  const router = useRouter();
+  const storedRecipe = sessionStorage.getItem('recipeResult');
+  const [recipeNullWarning, SetRecipeNullWaring] = useState(false);
+  let recipe: RecipeResult | null = null;
+
+  if (storedRecipe) {
+    recipe = JSON.parse(storedRecipe);
+  }
+
+  if (!recipe) {
+    console.error('This error is from RecipePage because recipe has null');
+    SetRecipeNullWaring(true);
+    router.push('/');
+  }
+
   return (
     <>
       <Logo />
 
-      {/* Recipo Searchbar */}
-      {/* <Searchbar handleSubmit={handleSubmit} /> */}
+      {/* Display result after finishing the process from back-end part */}
+      {recipe && <Result data={recipe} />}
+
+      <NotificationModal
+        open={recipeNullWarning}
+        onOpenChange={SetRecipeNullWaring}
+        titleImage={
+          <Image
+            src="/icons/embarrassedIcon.png"
+            alt="Invalid URL Notification Icon"
+            width={40}
+            height={40}
+          />
+        }
+        modalTitle="Invalid YouTube Link"
+        description={
+          'Please enter a valid YouTube video link.\n\nIf the video is valid, it may not have a transcript or enough spoken content to generate a recipe.'
+        }
+      />
     </>
   );
 }
